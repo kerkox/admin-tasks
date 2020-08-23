62b2276c59d931f6130a1056ff0d57185771028a
Paul Cortes <paulkerg@gmail.com>
2020-08-23 15:31:23 -0500

    add dotenv

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import Server from './server/server';
 import Router from './routes/api/index';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose'
+require('dotenv').confgi();
 
 const port = Number(process.env.PORT) || 3000;
 const server = Server.init(port);
@@ -9,7 +10,7 @@ server.app.use(bodyParser.urlencoded({ extended: false }))
 server.app.use(bodyParser.json())
 server.app.use(Router);
 
-
+const mongodbURL = process.env.MOGO_URI || 'mongodb://localhost/tasks'
 
 server.start(() => {
   console.log(`correindo en el puerto ${port}`);
